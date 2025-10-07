a2d09d1c15c7f3ace144f75e91f0f2fb0b775e37
R0X <patryk.bieganski1003@gmail.com>
2025-10-07 18:19:40 +0200

    Update fedstats.py

--- tg_tools/fedstats.py
+++ tg_tools/fedstats.py
@@ -66,7 +66,7 @@ async def query_single_bot(bot: BOT, bot_id: int, user_to_check: User) -> tuple[
             file_message = await find_latest_file_in_history(bot, bot_id)
 
             if response.text == "You can only use fed commands once every 5 minutes":
-                result_text = f"<b>• {bot_info.first_name}:</b> You can only use fed commands once every 5 minutes."
+                result_text = f"<b>• {bot_info.first_name}:</b> <blockquote expandable>You can only use fed commands once every 5 minutes.</blockquote>"
             elif file_message:
                 result_text = f"<b>• {bot_info.first_name}:</b> Bot sent a file with the full ban list. Sending..."
             else:
