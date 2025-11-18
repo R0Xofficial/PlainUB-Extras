c33eabd2dddfb8ed1b15e433204fdb21d84db466
R0X <patryk.bieganski1003@gmail.com>
2025-11-18 21:37:50 +0100

    Update rotate.py

--- tools/rotate.py
+++ tools/rotate.py
@@ -111,7 +111,6 @@ async def rotate_handler(bot: BOT, message: Message):
             await bot.send_video(message.chat.id, modified_path, caption=caption, reply_parameters=reply_params)
         
         await progress_message.delete()
-        await message.delete()
 
     except Exception as e:
         error_text = f"<b>Error:</b> Could not rotate media.\n<code>{html.escape(str(e))}</code>"
