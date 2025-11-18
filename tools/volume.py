9e337c9a3a072944db2dc3bb7556bc5563772362
R0X <patryk.bieganski1003@gmail.com>
2025-11-18 21:39:08 +0100

    Update volume.py

--- tools/volume.py
+++ tools/volume.py
@@ -108,7 +108,6 @@ async def volume_handler(bot: BOT, message: Message):
             await bot.send_audio(message.chat.id, modified_path, caption=caption, reply_parameters=reply_params)
         
         await progress_message.delete()
-        await message.delete()
 
     except Exception as e:
         error_text = f"<b>Error:</b> Could not change volume.\n<code>{html.escape(str(e))}</code>"
