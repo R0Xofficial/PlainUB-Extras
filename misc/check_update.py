8426961b845efcb04f27a1e5645335ab412673b6
R0X <patryk.bieganski1003@gmail.com>
2025-11-18 20:23:44 +0100

    Update check_update.py

--- misc/check_update.py
+++ misc/check_update.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message, LinkPreviewOptions
 
 from app import BOT, bot
 
-from app.modules.repo_config import REPO_OWNER, REPO_NAME
+from app.modules.repo import REPO_OWNER, REPO_NAME
 
 REPO_API_URL = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}"
 
