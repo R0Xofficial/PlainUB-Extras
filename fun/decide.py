95f64c93463b1226a1560b2f8bf3598369909c95
R0X <patryk.bieganski1003@gmail.com>
2025-11-19 12:19:31 +0100

    Update decide.py

--- fun/decide.py
+++ fun/decide.py
@@ -3,6 +3,8 @@ from pyrogram.types import Message
 
 from app import BOT, bot
 
+from app.modules.settings import TINY_TIMEOUT, SMALL_TIMEOUT, MEDIUM_TIMEOUT, LONG_TIMEOUT, VERY_LONG_TIMEOUT, LARGE_TIMEOUT
+
 DECIDE_RESPONSES = [
     "Yes, definitely.", "No, absolutely not.", "Maybe, think about it.",
     "The answer is unclear, try again.", "Go for it!", "I wouldn't recommend it.",
@@ -23,7 +25,7 @@ async def decide_handler(bot: BOT, message: Message):
         await message.reply(
             "**What should I decide on?**\n"
             "Usage: `.decide [your question]`",
-            del_in=8
+            del_in=MEDIUM_TIMEOUT
         )
         return
 
