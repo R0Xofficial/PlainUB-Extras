3bc6688b68ba7729b58e911535adba651fcb5f46
R0X <patryk.bieganski1003@gmail.com>
2025-11-19 12:15:39 +0100

    Update ascii.py

--- fun/ascii.py
+++ fun/ascii.py
@@ -4,12 +4,13 @@ from pyrogram.enums import ParseMode
 
 from app import BOT, Message, bot
 
+from app.modules.settings import TINY_TIMEOUT, SMALL_TIMEOUT, MEDIUM_TIMEOUT, LONG_TIMEOUT, VERY_LONG_TIMEOUT, LARGE_TIMEOUT
 
 @bot.add_cmd(cmd="ascii")
 async def ascii(bot: BOT, message: Message):
     text = message.input
     if not text:
-        await message.reply("What am I supposed to say?", del_in=8)
+        await message.reply("What am I supposed to say?", del_in=MEDIUM_TIMEOUT)
         return
 
     ascii_text = pyfiglet.figlet_format(text)
