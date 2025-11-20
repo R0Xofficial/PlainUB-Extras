71e86777e974b51e746e4917715bd14cd6b41d34
R3QU3R <r3qu3r.remigiusz@gmail.com>
2025-11-20 13:21:05 +0100

    Update xiaomi.py

--- android/xiaomi.py
+++ android/xiaomi.py
@@ -73,7 +73,7 @@ async def codename_handler(bot: BOT, message: Message):
     if matches:
         header = f"<b>Found {len(matches)} matching devices:</b>"
         
-        formatted_lines = [f"<code>{safe_escape(name)}</code> is <b>{safe_escape(codename)}</b>" for codename, name in matches.items()]
+        formatted_lines = [f"<b>{safe_escape(name)}</b> is <code>{safe_escape(codename)}</code>" for codename, name in matches.items()]
         blockquote_content = "\n\n".join(sorted(formatted_lines))
         
         full_res = f"{header}\n<blockquote expandable>{blockquote_content}</blockquote>"
@@ -119,7 +119,7 @@ async def miui_handler(bot: BOT, message: Message):
                 target_codename = list(possible_devices.keys())[0]
             elif len(possible_devices) > 1:
                 header = f"<b>Query is ambiguous. Found {len(possible_devices)} devices:</b>"
-                formatted_lines = [f"<code>{safe_escape(name)}</code> is <b>{safe_escape(codename)}</b>" for codename, name in possible_devices.items()]
+                formatted_lines = [f"<b>{safe_escape(name)}</b> is <code>{safe_escape(codename)}</code>" for codename, name in possible_devices.items()]
                 blockquote_content = "\n\n".join(sorted(formatted_lines))
                 footer = "\nPlease try again with a more specific name or use the exact codename."
                 full_res = f"{header}\n<blockquote expandable>{blockquote_content}</blockquote>{footer}"
